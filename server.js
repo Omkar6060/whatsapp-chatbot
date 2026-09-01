b7867698d627d7b4d5bd9eff58cb290713b28da6
omkar6060 <omkarcha6060@gmail.com>
2026-09-02 00:18:21 +0530

    Switch to Telegram, pin bot API version to avoid v2 break

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running'));
+app.listen(process.env.PORT || 3000, () => console.log('Health check server up'));
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
